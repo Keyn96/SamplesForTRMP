bc3da9e3563f6bf25383517e1025fbf03213460c
Vlad <danilov.wlad@yandex.ru>
2023-07-24 14:14:32 +0300

    Work 3

--- app/src/main/java/com/example/myfirstapplicationj/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplicationj/MainActivity.java
@@ -1,176 +1,34 @@
 package com.example.myfirstapplicationj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    Button mButton0, mButton1, mButton2, mButton3, mButton4, mButton5, mButton6, mButton7, mButton8,
-            mButton9, mButtonPoint, mButtonAdd, mButtonSub, mButtonDiv, mButtonMul, mButtonEq;
-    EditText mEditText;
-    float mValueOne, mValueTwo;
-    boolean mAddition, mSubtract, mMultiplication, mDivision;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton0 = (Button) findViewById(R.id.button0);
-        mButton1 = (Button) findViewById(R.id.button1);
-        mButton2 = (Button) findViewById(R.id.button2);
-        mButton3 = (Button) findViewById(R.id.button3);
-        mButton4 = (Button) findViewById(R.id.button4);
-        mButton5 = (Button) findViewById(R.id.button5);
-        mButton6 = (Button) findViewById(R.id.button6);
-        mButton7 = (Button) findViewById(R.id.button7);
-        mButton8 = (Button) findViewById(R.id.button8);
-        mButton9 = (Button) findViewById(R.id.button9);
-        mButtonPoint = (Button) findViewById(R.id.buttonPoint);
-        mButtonAdd = (Button) findViewById(R.id.buttonAdd);
-        mButtonSub = (Button) findViewById(R.id.buttonSub);
-        mButtonMul = (Button) findViewById(R.id.buttonMul);
-        mButtonDiv = (Button) findViewById(R.id.buttonDiv);
-        mButtonEq = (Button) findViewById(R.id.buttonEq);
-        mEditText = (EditText) findViewById(R.id.editText1);
-        mButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "1");
-            }
-        });
-        mButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "2");
-            }
-        });
-        mButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "3");
-            }
-        });
-        mButton4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "4");
-            }
-        });
-        mButton5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "5");
-            }
-        });
-        mButton6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "6");
-            }
-        });
-        mButton7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "7");
-            }
-        });
-        mButton8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "8");
-            }
-        });
-        mButton9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "9");
-            }
-        });
-        mButton0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "0");
-            }
-        });
-        mButtonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mEditText == null) {
-                    mEditText.setText("");
-                } else {
-                    mValueOne = Float.parseFloat(mEditText.getText() + "");
-                    mAddition = true;
-                    mEditText.setText(null);
-                }
-            }
-        });
-        mButtonSub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mEditText == null) {
-                    mEditText.setText("");
-                } else {
-                    mValueOne = Float.parseFloat(mEditText.getText() + "");
-                    mSubtract = true;
-                    mEditText.setText(null);
-                }
-            }
-        });
-        mButtonMul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mEditText == null) {
-                    mEditText.setText("");
-                } else {
-                    mValueOne = Float.parseFloat(mEditText.getText() + "");
-                    mMultiplication = true;
-                    mEditText.setText(null);
-                }
-            }
-        });
-        mButtonDiv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mEditText == null) {
-                    mEditText.setText("");
-                } else {
-                    mValueOne = Float.parseFloat(mEditText.getText() + "");
-                    mDivision = true;
-                    mEditText.setText(null);
-                }
-            }
-        });
-        mButtonEq.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueTwo = Float.parseFloat(mEditText.getText() + "");
-                if (mAddition == true) {
-                    mEditText.setText(mValueOne + mValueTwo + "");
-                    mAddition = false;
-                }
-                if (mSubtract == true) {
-                    mEditText.setText(mValueOne - mValueTwo + "");
-                    mSubtract = false;
-                }
-                if (mMultiplication == true) {
-                    mEditText.setText(mValueOne * mValueTwo + "");
-                    mMultiplication = false;
-                }
-                if (mDivision == true) {
-                    mEditText.setText(mValueOne / mValueTwo + "");
-                    mDivision = false;
-                }
-            }
-        });
-        mButtonPoint.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + ".");
-            }
-        });
+        Button mButton1 = findViewById(R.id.button);
+        TextView mTextView = findViewById(R.id.textView);
+        mTextView.setText((String)getIntent().getSerializableExtra("message"));
+        mButton1.setOnClickListener(this);
 
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void onClick(View view) {
+        if (view.getId() == R.id.button) {
+            Intent intent = new Intent(getApplicationContext(), ActivityTwo.class);
+            startActivity(intent);
+        }
+    }
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
     }
 }
