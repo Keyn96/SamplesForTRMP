c6f716fb1e2d936f532e578e2f052e1526838d0e
Vlad <danilov.wlad@yandex.ru>
2023-07-25 14:09:16 +0300

    Getting started on Work 4.

--- app/src/main/java/com/example/myfirstapplicationj/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplicationj/MainActivity.java
@@ -1,34 +1,92 @@
 package com.example.myfirstapplicationj;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    TextView mAccelerometerX;
+    TextView mAccelerometerY;
+    TextView mAccelerometerZ;
+    TextView mMagneticX;
+    TextView mMagneticY;
+    TextView mMagneticZ;
+    TextView mProximity;
+    TextView mLight;
+    SensorManager sensorManager;
+    Sensor mAccelerometerSensor;
+    Sensor mProximitySensor;
+    Sensor mMagneticSensor;
+    Sensor mLightSensor;
+    float mMaxValue;
+    float mValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button mButton1 = findViewById(R.id.button);
-        TextView mTextView = findViewById(R.id.textView);
-        mTextView.setText((String)getIntent().getSerializableExtra("message"));
-        mButton1.setOnClickListener(this);
-
+        mAccelerometerX = (TextView)findViewById(R.id.textView2);
+        mAccelerometerY = (TextView)findViewById(R.id.textView3);
+        mAccelerometerZ = (TextView)findViewById(R.id.textView4);
+        mMagneticX = (TextView)findViewById(R.id.textView6);
+        mMagneticY = (TextView)findViewById(R.id.textView7);
+        mMagneticZ = (TextView)findViewById(R.id.textView8);
+        mProximity = (TextView)findViewById(R.id.textView10);
+        mLight = (TextView)findViewById(R.id.textView12);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mProximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mLightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mMaxValue = mLightSensor.getMaximumRange();
     }
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            mAccelerometerX.setText(Float.toString(event.values[0]));
+            mAccelerometerY.setText(Float.toString(event.values[1]));
+            mAccelerometerZ.setText(Float.toString(event.values[2]));
+        }
+        if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD){
+            mMagneticX.setText(Float.toString(event.values[0]));
+            mMagneticY.setText(Float.toString(event.values[1]));
+            mMagneticZ.setText(Float.toString(event.values[2]));
+        }
+        if(event.sensor.getType()==Sensor.TYPE_PROXIMITY){
+            mProximity.setText(Float.toString(event.values[0]));
+        }
+        if(event.sensor.getType()==Sensor.TYPE_LIGHT){
 
-    @SuppressLint("NonConstantResourceId")
-    public void onClick(View view) {
-        if (view.getId() == R.id.button) {
-            Intent intent = new Intent(getApplicationContext(), ActivityTwo.class);
-            startActivity(intent);
+            mLight.setText(Float.toString(event.values[0]));
+            mValue =event.values[0];
+            WindowManager.LayoutParams layout = getWindow().getAttributes();
+            layout.screenBrightness = (int)(255f* mValue / mMaxValue);
+            getWindow().setAttributes(layout);
         }
     }
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(this, mAccelerometerSensor, sensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, mMagneticSensor, sensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, mProximitySensor, sensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, mLightSensor, sensorManager.SENSOR_DELAY_FASTEST);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        super.onStop();
+        sensorManager.unregisterListener(this, mAccelerometerSensor);
+        sensorManager.unregisterListener(this, mMagneticSensor);
+        sensorManager.unregisterListener(this, mProximitySensor);
+        sensorManager.unregisterListener(this, mLightSensor);
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        sensorManager.unregisterListener(this, mLightSensor);
     }
 }
