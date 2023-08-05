63276776e86e97b0ba729bada80a4177ff54adc7
Vlad <danilov.wlad@yandex.ru>
2023-08-05 15:30:14 +0300

    End work on 5 LR

--- app/src/main/java/com/example/myfirstapplicationj/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplicationj/MainActivity.java
@@ -1,97 +1,107 @@
 package com.example.myfirstapplicationj;
 
 import android.annotation.SuppressLint;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.view.WindowManager;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    TextView mAccelerometerX;
-    TextView mAccelerometerY;
-    TextView mAccelerometerZ;
-    TextView mMagneticX;
-    TextView mMagneticY;
-    TextView mMagneticZ;
-    TextView mProximity;
-    TextView mLight;
-    SensorManager sensorManager;
-    Sensor mAccelerometerSensor;
-    Sensor mProximitySensor;
-    Sensor mMagneticSensor;
-    Sensor mLightSensor;
-    float mMaxValue;
-    float mValue;
-    @SuppressLint("CutPasteId")
+public class MainActivity extends AppCompatActivity {
+    final String LOG_TAG = "myLogs";
+    Button mBtnAdd, mBtnRead, mBtnClear;
+    EditText mEdtName, mEdtEmail;
+    DBHelper mDbHelper;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAccelerometerX = (TextView)findViewById(R.id.textView2);
-        mAccelerometerY = (TextView)findViewById(R.id.textView3);
-        mAccelerometerZ = (TextView)findViewById(R.id.textView4);
-        mMagneticX = (TextView)findViewById(R.id.textView6);
-        mMagneticY = (TextView)findViewById(R.id.textView7);
-        mMagneticZ = (TextView)findViewById(R.id.textView8);
-        mProximity = (TextView)findViewById(R.id.textView10);
-        mLight = (TextView)findViewById(R.id.textView12);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mAccelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mMagneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        mProximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        mLightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        mMaxValue = mLightSensor.getMaximumRange();
+        mBtnAdd = (Button) findViewById(R.id.buttonAdd);
+        mBtnAdd.setOnClickListener(this:: onClick);
+        mBtnRead = (Button) findViewById(R.id.buttonRead);
+        mBtnRead.setOnClickListener(this :: onClick);
+        mBtnClear = (Button) findViewById(R.id.buttonClear);
+        mBtnClear.setOnClickListener(this :: onClick);
+        mEdtName = (EditText) findViewById(R.id.editTextName);
+        mEdtEmail = (EditText) findViewById(R.id.editTextEmail);
+        mDbHelper = new DBHelper(this);
     }
-    @SuppressLint("SetTextI18n")
-    @Override
-    public void onSensorChanged(SensorEvent event)
-    {
-        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
-            mAccelerometerX.setText(Float.toString(event.values[0]));
-            mAccelerometerY.setText(Float.toString(event.values[1]));
-            mAccelerometerZ.setText(Float.toString(event.values[2]));
+    @SuppressLint("NonConstantResourceId")
+    public void onClick(View v) {
+        // создаем объект для данных
+        ContentValues cv = new ContentValues();
+        // получаем данные из полей ввода
+        String name = mEdtName.getText().toString();
+        String email = mEdtEmail.getText().toString();
+        // подключаемся к БД
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        if (v.getId() == R.id.buttonAdd){
+            Log.d(LOG_TAG, "--- Insert in mytable: ---");
+            // подготовим данные для вставки в виде пар: наименование столбца -
+            // значение
+            cv.put("name", name);
+            cv.put("email", email);
+            // вставляем запись и получаем ее ID
+            long rowID = db.insert("mytable", null, cv);
+            Log.d(LOG_TAG, "row inserted, ID = " + rowID);
         }
-        if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD){
-            mMagneticX.setText(Float.toString(event.values[0]));
-            mMagneticY.setText(Float.toString(event.values[1]));
-            mMagneticZ.setText(Float.toString(event.values[2]));
+        if (v.getId() == R.id.buttonRead){
+            Log.d(LOG_TAG, "--- Rows in mytable: ---");
+            // делаем запрос всех данных из таблицы mytable, получаем Cursor
+            Cursor c = db.query("mytable", null, null, null, null, null, null);
+            // ставим позицию курсора на первую строку выборки
+            // если в выборке нет строк, вернется false
+            if (c.moveToFirst()) {
+                // определяем номера столбцов по имени в выборке
+                int idColIndex = c.getColumnIndex("id");
+                int nameColIndex = c.getColumnIndex("name");
+                int emailColIndex = c.getColumnIndex("email");
+                do {
+                    // получаем значения по номерам столбцов и пишем все в лог
+                    Log.d(LOG_TAG,
+                            "ID = " + c.getInt(idColIndex) + ", name = "
+                                    + c.getString(nameColIndex) + ", email = "
+                                    + c.getString(emailColIndex));
+                    // переход на следующую строку
+                    // а если следующей нет (текущая - последняя), то false -
+                    // выходим из цикла
+                } while (c.moveToNext());
+            } else
+                Log.d(LOG_TAG, "0 rows");
+            c.close();
         }
-        if(event.sensor.getType()==Sensor.TYPE_PROXIMITY){
-            mProximity.setText(Float.toString(event.values[0]));
+        if (v.getId() == R.id.buttonClear){
+            Log.d(LOG_TAG, "--- Clear mytable: ---");
+            // удаляем все записи
+            int clearCount = db.delete("mytable", null, null);
+            Log.d(LOG_TAG, "deleted rows count = " + clearCount);
         }
-        if(event.sensor.getType()==Sensor.TYPE_LIGHT){
+        // закрываем подключение к БД
+        mDbHelper.close();
+    }
+    class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(Context context) {
+            // конструктор суперкласса
+            super(context, "myDB", null, 1);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+            // создаем таблицу с полями
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + "email text" + ");");
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+    }
 
-            mLight.setText(Float.toString(event.values[0]));
-            mValue =event.values[0];
-            WindowManager.LayoutParams layout = getWindow().getAttributes();
-            layout.screenBrightness = (int)(255f* mValue / mMaxValue);
-            getWindow().setAttributes(layout);
-        }
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        sensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        sensorManager.registerListener(this, mMagneticSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        sensorManager.registerListener(this, mProximitySensor, SensorManager.SENSOR_DELAY_FASTEST);
-        sensorManager.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        super.onStop();
-        sensorManager.unregisterListener(this, mAccelerometerSensor);
-        sensorManager.unregisterListener(this, mMagneticSensor);
-        sensorManager.unregisterListener(this, mProximitySensor);
-        sensorManager.unregisterListener(this, mLightSensor);
-    }
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-        sensorManager.unregisterListener(this, mLightSensor);
-    }
 }
