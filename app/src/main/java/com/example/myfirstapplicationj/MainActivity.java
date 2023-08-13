decac86da89c8fcf8bc57af54f1d6aabd7c909fb
Vlad <danilov.wlad@yandex.ru>
2023-08-13 16:29:25 +0300

    Second Phase LR6

--- app/src/main/java/com/example/myfirstapplicationj/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplicationj/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,14 +17,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(new DrawView(this));
     }
 
-    static class DrawView extends View {
+    static class DrawView extends SurfaceView implements SurfaceHolder.Callback {
+        private DrawThread drawThread;
+
         public DrawView(Context context) {
+
             super(context);
+            getHolder().addCallback(this);
         }
 
         @Override
-        protected void onDraw(Canvas canvas) {
-            canvas.drawColor(Color.GREEN);
+        public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder holder) {
+            drawThread = new DrawThread(getHolder());
+            drawThread.setRunning(true);
+            drawThread.start();
+
+        }
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+            boolean retry = true;
+            drawThread.setRunning(false);
+            while (retry) {
+                try {
+                    drawThread.join();
+                    retry = false;
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+
+        static class DrawThread extends Thread {
+            private boolean running = false;
+            private SurfaceHolder surfaceHolder;
+
+            public DrawThread(SurfaceHolder surfaceHolder) {
+                this.surfaceHolder = surfaceHolder;
+            }
+
+            public void setRunning(boolean running) {
+                this.running = running;
+            }
+
+            @Override
+            public void run() {
+                Canvas canvas;
+                while (running) {
+                    canvas = null;
+                    try {
+                        canvas = surfaceHolder.lockCanvas(null);
+                        if (canvas == null)
+                            continue;
+                        canvas.drawColor(Color.GREEN);
+                    } finally {
+                        if (canvas != null) {
+                            surfaceHolder.unlockCanvasAndPost(canvas);
+                        }
+                    }
+                }
+            }
         }
     }
 }
